5edd7fc94d95f8aa532806645a54be8902a498a2
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2025-10-18 21:36:35 +0530

    serach-agent-implementation

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain_openai import AzureChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [
+    TavilySearch()
+]
+llm = AzureChatOpenAI(
+    temperature=0,
+    model = "gpt-4o"
+)
+
+agent = create_agent(
+    model=llm,
+    tools=tools
+)
+
 def main():
-    print("Hello from langchain!")
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Serach for 3 job posting for an ai engineer the area of linkedin and list their results"    
+                }
+            ]
+        }
+    )
+
+    print(result)
 
 
 if __name__ == "__main__":
