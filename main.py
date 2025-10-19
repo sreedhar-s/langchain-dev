bb1e5297fbedc74f888c0cc11b61ae1f725023f9
sreedhar-s <gandikotasreedhar98@gmail.com>
2025-10-19 12:42:52 +0530

    added structured output for agent

--- main.py
+++ main.py
@@ -5,34 +5,18 @@ load_dotenv()
 from langchain.agents import create_agent
 from langchain_openai import AzureChatOpenAI
 from langchain_tavily import TavilySearch
+from langchain.agents.structured_output import ToolStrategy
 
-tools = [
-    TavilySearch()
-]
-llm = AzureChatOpenAI(
-    temperature=0,
-    model = "gpt-4o"
-)
+from schemas import AgentResponse
 
-agent = create_agent(
-    model=llm,
-    tools=tools
-)
+tools = [TavilySearch()]
+llm = AzureChatOpenAI(temperature=0, model="gpt-4o")
+
+agent = create_agent(model=llm, tools=tools, response_format=ToolStrategy(AgentResponse))
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "Serach for 3 job posting for an ai engineer the area of linkedin and list their results"    
-                }
-            ]
-        }
-    )
-
+    result = agent.invoke({"messages": [{"role": "user", "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"}]})
     print(result)
-
 
 if __name__ == "__main__":
     main()
