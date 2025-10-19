91adf05412e256501fda36f2bcb86646764f5a4c
sreedhar-s <gandikotasreedhar98@gmail.com>
2025-10-19 20:23:36 +0530

    custom tool implementation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,17 +7,28 @@ from langchain.agents import create_agent
 from langchain_openai import AzureChatOpenAI
 from langchain_tavily import TavilySearch
 from langchain.agents.structured_output import ToolStrategy
+from langchain.tools import tool
 
 from schemas import AgentResponse
 
-tools = [TavilySearch()]
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
+    return len(text)
+
+tools = [TavilySearch(), get_text_length]
 llm = AzureChatOpenAI(temperature=0, model="gpt-4o")
+
 
 agent = create_agent(model=llm, tools=tools, response_format=ToolStrategy(AgentResponse))
 
 def main():
-    result = agent.invoke({"messages": [{"role": "user", "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"}]})
-    print(result)
+    result = agent.invoke({"messages": [{"role": "user", "content": "What is the length of the word: Langchain"}]})
+    # print(result['messages'])
+    print(result['structured_response'])
 
 if __name__ == "__main__":
     main()
